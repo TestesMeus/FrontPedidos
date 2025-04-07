a4a4fd8a72f6f5ea8ba5fa321e60ebe51032980a
TestesMeus <pedidos.bruno.barral@gmail.com>
2025-04-07 14:05:25 -0300

    Update CarrinhoMateriais.jsx

--- src/components/CarrinhoMateriais.jsx
+++ src/components/CarrinhoMateriais.jsx
@@ -227,7 +227,9 @@ const TopBar = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  margin-bottom: 1rem;
+  flex-wrap: wrap;
+  gap: 0.5rem;
 `;
+
 
 export default CarrinhoMateriais;
